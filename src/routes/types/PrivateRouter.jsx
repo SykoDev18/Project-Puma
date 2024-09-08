f14f6cc028bf39d93e9cae20c592ff8db35990cd
Marco Miranda <marcomiranda566@gmail.com>
2024-09-07 19:02:05 -0600

    Config2

---
+++ src/routes/types/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRouter = ({ children }) => {
+    const {state} = useLocation();
+    return 
+}
+
+export default PrivateRouter;
